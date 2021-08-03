5f0b197ba4afcbc9b87dc61db7a0c4ca46a93b23
fresh-patches <ggrodriguez.dev@gmail.com>
2021-08-02 23:10:12 -0700

    added get comments from a video.

--- youtubescraper.py
+++ youtubescraper.py
@@ -5,11 +5,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd 
-import time, json, datetime
+import time, json, datetime, os
 
 
 class YouTubeScraper:
-    def __init__(self, driver_path, headless=True,  wait=60):
+    def __init__(self, driver_path, headless=True,  wait=30):
         """
         Get videos/data from YouTube.  
 
@@ -22,12 +22,12 @@ class YouTubeScraper:
         self._chrome_options = Options()
         if headless:
             self._chrome_options.add_argument("--headless")
-        else:
-            self._driver.maximize_window()
+  
         self._driver = webdriver.Chrome(options=self._chrome_options, executable_path=driver_path)
+        self._driver.maximize_window()
         self._wait = WebDriverWait(self._driver, wait)
 
-    def search(self, search_query='rickroll', meta_data=False, save=False, scale=0):
+    def search(self, search_query='rickroll', meta_data=False, save=False, directory='scrapes', scale=0):
         """
         Search for videos on YouTube 
 
@@ -35,14 +35,22 @@ class YouTubeScraper:
             search_query: a str type containing search query for YouTube. 
             meta_data: a boolean type,  True : return  a list of dcts with metadata.
                                         False: return a list of videoIDs
-                                       
+            comments: generate a csv of comments in a video.
             save: a boolean type,  True: save DataFrame to local dir as a .csv file. 
-            scale: a int type, Determines the scale of how many videos are visible to 
+            seachScale: a int type, Determines the scale of how many videos are visible to 
                   Chrome by scrolling by this amount.
+            commentScale: a int type, Determines the scale of how many comments are visible.
         Returns:
             A list of videoIDs  or panda DataFrame containing metadata of each videoID
 
         """
+        cur_dir = os.getcwd()
+        path = os.path.join(cur_dir, directory)
+
+
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
         url = "https://www.youtube.com/results?search_query="+search_query
         self._driver.get(url)
         for i in range(scale): 
@@ -56,24 +64,13 @@ class YouTubeScraper:
                 video_id = video_id.replace("https://www.youtube.com/watch?v=","")
                 videos.append(video_id)
 
-        if meta_data:
-            results = []
-            for video_id in videos:
-                try:
-                    data = self.get_video_meta_data(video_id)
-                except Exception as e:
-                    print("Error Detected! ", e)
-                else:
-                    print(data)
-                    results.append(data)
-                    time.sleep(1)
-            videos_dataframe = pd.DataFrame(results, columns=['videoID','likes','dislikes','viewCount','uploadDate','category']) 
-
-            if save:
-                videos_dataframe.to_csv(f"youtube_webscrape_{search_query.replace(' ','-')}.csv")
+        if meta_data and len(videos) > 0:
+            videos_dataframe = self.create_search_dataframe(videos) 
+            if save and not videos_dataframe.empty:
+                videos_dataframe.to_csv(os.path.join(path,f"query_{search_query.replace(' ','-')}_results.csv"))
+                print("Successfully saved search results DataFrame!")
 
             return videos_dataframe
-
         else:
             return videos
 
@@ -88,6 +85,7 @@ class YouTubeScraper:
             data: a dictionary type, containing video metadata.
 
         """
+
         url = "https://www.youtube.com/watch?v="+videoID
         self._driver.get(url)
         data = {}
@@ -127,6 +125,58 @@ class YouTubeScraper:
         data['uploadDate'] = datetime.datetime.strptime(dump['microformat']['playerMicroformatRenderer']['uploadDate'],"%Y-%m-%d")
         return data
 
+    def get_comments(self, videoID, directory="comments", save=False, scale=3):
+        comments =[]
+        cur_dir = os.getcwd()
+        path = os.path.join(cur_dir, directory)
+
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
+        url = "https://www.youtube.com/watch?v="+videoID
+        self._driver.get(url)  
+        for i in range(scale): 
+            time.sleep(1)
+            self._wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.PAGE_DOWN) 
+
+        try:
+            for comment in self._wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "style-scope ytd-comment-renderer"))):
+                post = {}
+                post['likes'] = 0 
+                post['author'] = "" 
+                post['content'] = ""
+            
+                post['author'] = comment.find_element_by_id("author-text").text
+                if post['author'] == "":
+                    post['author'] = comment.find_element_by_id("text").text
+                post['content'] = comment.find_element_by_id("content-text").text
+                post['likes']  = comment.find_element_by_id("vote-count-middle").text
+                comments.append(post)
+                print(post)
+            comments_df = pd.DataFrame(comments, columns=['author','likes','content'])
+            print(comments_df)
+
+        except Exception as e:
+            print(f"Unable to retrieve comments from {videoID}.")
+        else:
+            if save:
+                save_path = os.path.join(path, f"videoID_{videoID}.csv")
+                comments_df.to_csv(save_path)
+
+
+    def create_search_dataframe(self, videos):
+        results = []
+        for video_id in videos:
+            try:
+                data = self.get_video_meta_data(video_id)
+            except Exception as e:
+                print("Error Detected! ", e)
+            else:
+                print(data)
+                results.append(data)
+                time.sleep(1)
+        return pd.DataFrame(results, columns=['videoID','likes','dislikes','viewCount','uploadDate','category'])
+
     def close(self):
         """
         Close Chrome Session 
@@ -139,9 +189,13 @@ def main():
     Webscrape for meme videos on YouTube.
     Print Results. 
     """
-    yt = YouTubeScraper('./chromedriver')
+    yt = YouTubeScraper('./chromedriver', headless=False)
     print("Scraping...")
     result = yt.search(search_query="Nature", meta_data=True, save=True)
+    for i in range(len(result['videoID'])):
+        yt.get_comments(result['videoID'][i], save=True)
+      
+   
     yt.close()
     print(result)
     print("Web Scrape Complete!")
